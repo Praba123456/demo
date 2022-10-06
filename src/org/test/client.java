2e6f5e2616a732ce1a1964a0e56e6919a0a2230d
Praba123456 <prbshnkr@gmail.com>
2022-10-06 21:22:12 +0530

    add changes

--- src/org/test/client.java
+++ src/org/test/client.java
@@ -10,8 +10,10 @@ public static void main(String[] args) {
 	a[1]=20;
 	a[2]=30;
 	a[3]=40;
+	a[4]=50;
+	
 	int length = a.length;
-	System.out.println("length:" +length);
+	System.out.println("length");
 	
 	for (int i = 0; i < a.length; i++) {
 		System.out.println(a[i]);
